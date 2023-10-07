5d77129bc999524ac7c0829ba193dad840ced5bc
natal <svetnata74@mail.ru>
2023-10-07 09:32:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# qaguru_hm15
+Hello! This is my homework qa guru lesson 15 potok 7
---
+++ tests/test_azalia_stagging.py
@@ -0,0 +1,52 @@
+import allure
+from selene import have, browser
+from selene.support.shared import browser
+
+
+@allure.title("Проверка что избранное пустое изначально")
+def test_check_empty_favorites():
+    browser.open('')
+    browser.element('[data-testid="banner-mobile-app-close"]').click()  # close advertisment
+    browser.all('[data-testid="favorites"]')[1].click()  # TODO NATALIE FILTER VISIBLE
+    browser.element('[class^="Favorites_empty__title__"]').should(have.exact_text('Здесь пока пусто'))
+
+
+@allure.title("Проверка что есть 2 города на главной странице")
+def test_check_2_cities():
+    browser.open('https://staging.azalianow.shop/')
+    browser.element('[data-testid="banner-mobile-app-close"]').click()  # close advertisment
+    browser.element('[data-testid="geo-toggler"]').click()  # click listbox cities
+    browser.all('[data-testid="geo-item"]')[0].should(have.text('Москва и Московская область'))
+    browser.all('[data-testid="geo-item"]')[1].should(have.text('Нижний Новгород и Нижегородская область'))
+
+
+@allure.title("Проверка что корзина изначально пустая")
+def test_check_bin():
+    browser.open('https://staging.azalianow.shop/')
+    browser.element('[data-testid="banner-mobile-app-close"]').click()  # close advertisment
+    browser.all('[data-testid="checkout"]')[1].click()  # click bin
+    browser.element('[class^="Favorites_empty__title__"]').should(have.text('В корзине пусто'))
+
+
+@allure.title("Проверка что пользователь изначально не авторизован")
+def test_check_user_not_authorised():
+    browser.open('')
+    browser.element('[data-testid="banner-mobile-app-close"]').click()  # close advertisment
+    browser.all('[data-testid="open-login-modal"]')[1].should(have.text('Войти'))
+
+
+@allure.title("Проверка что поиск работает и ищет")
+def test_check_search_rozi():
+    browser.open('')
+    browser.element('[data-testid="banner-mobile-app-close"]').click()  # close advertisment
+    browser.element('[inputmode="search"]').type("Розы").press_enter()
+    browser.element('[class^="SearchCatalog_title__').should(have.text('розы'))
+    browser.all('[class^="ProductCard_name__"]')[1].should(have.text("роз"))
+
+
+@allure.title("Добавление любого товара в корзину и проверка что он добавлен")
+def test_add_to_bin():
+    browser.open('')
+    browser.element('[data-testid="banner-mobile-app-close"]').click()  # close advertisment
+    browser.all('[class^="ProductCard_cartButton__"]')[0].click()
+    browser.all('[class ^="ProductsListItem_wrapper__"]')[0].should(have.text('1'))
